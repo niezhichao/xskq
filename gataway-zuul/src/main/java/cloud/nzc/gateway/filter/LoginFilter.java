5e3f39ad99fd173c07e14343015ee5131d21ba92
niezhichao <727841296@qq.com>
2019-06-27 17:07:35 +0800

    submit zuulFilter

---
+++ gataway-zuul/src/main/java/cloud/nzc/gateway/filter/LoginFilter.java
@@ -0,0 +1,46 @@
+package cloud.nzc.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.ZuulFilterResult;
+import com.netflix.zuul.exception.ZuulException;
+
+public class LoginFilter extends ZuulFilter {
+    public LoginFilter() {
+        super();
+    }
+
+    @Override
+    public boolean isStaticFilter() {
+        return super.isStaticFilter();
+    }
+
+    @Override
+    public String disablePropertyName() {
+        return super.disablePropertyName();
+    }
+
+    @Override
+    public boolean isFilterDisabled() {
+        return super.isFilterDisabled();
+    }
+
+    @Override
+    public ZuulFilterResult runFilter() {
+        return super.runFilter();
+    }
+
+    @Override
+    public int compareTo(ZuulFilter filter) {
+        return super.compareTo(filter);
+    }
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+}
