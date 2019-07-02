185210f5f120c283b36331971e7e7ca71fa67a62
niezhichao <727841296@qq.com>
2019-07-02 20:33:28 +0800

    modify new file

---
+++ user-service/src/main/java/cloud/nzc/user/dao/AppPermissinDao.java
@@ -0,0 +1,9 @@
+package cloud.nzc.user.dao;
+
+import cloud.nzc.model.mybatis.dao.BaseDao;
+import cloud.nzc.model.po.AppPermission;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AppPermissinDao extends BaseDao<AppPermission> {
+}
--- user-service/src/main/java/cloud/nzc/user/dao/UserMapper.java
+++ user-service/src/main/java/cloud/nzc/user/dao/UserMapper.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 public interface UserMapper extends BaseDao<UserPo> {
 
+
+
 }
